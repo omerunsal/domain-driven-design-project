c143eb17a7fd1392b98106e4bc5f6b4772c3d977
omerunsal <omerunsal94@outlook.com>
2023-06-23 17:48:33 +0300

    OrderItem.cs edit

--- Order.Domain/AggregateModels/OrderModels/OrderItem.cs
+++ Order.Domain/AggregateModels/OrderModels/OrderItem.cs
@@ -4,9 +4,9 @@ namespace Order.Domain.AggregateModels.OrderModels;
 
 public class OrderItem : BaseEntity
 {
-    public int Quantity { get; set; }
-    public decimal Price { get; set; }
-    public int ProductId { get; set; }
+    public int Quantity { get; private set; }
+    public decimal Price { get; private set; }
+    public int ProductId { get; private set; }
 
     public OrderItem(int quantity, decimal price, int productId)
     {
